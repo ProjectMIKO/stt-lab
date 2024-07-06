ca14c59f9782bfa72e97360a61ca2c4a8349a226
t3rryahn <t3rryahn.dev@gmail.com>
2024-07-06 12:13:04 +0900

    feat: 말하는 부분만 감지해서 잘라주는 기능

--- audio_enhancer/vad.py
+++ audio_enhancer/vad.py
@@ -1,19 +1,40 @@
 import torch
 import torchaudio
 from IPython.display import Audio
+import sys
+import time
 
-# Silero VAD 모델 다운로드
-model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad', model='silero_vad', force_reload=True)
-(get_speech_ttimestamps, save_audio, read_audio, VADIterator, collect_chunks) = utils
+def vad(input_file, output_filename):
+    start_time_vad = time.time()
 
-# 오디오 파일 로드
-wav = read_audio('voice_file/recording_fixed (1).wav')
+    # Silero VAD 모델 다운로드
+    model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad', model='silero_vad', force_reload=False)
+    (get_speech_ttimestamps, save_audio, read_audio, VADIterator, collect_chunks) = utils
 
-# VAD를 사용하여 말하는 구간 찾기
-speech_timestamps = get_speech_ttimestamps(wav, model)
+    # 오디오 파일 로드
+    wav = read_audio(input_file)
 
-# 말하는 구간만 추출
-speech_wav = collect_chunks(speech_timestamps, wav)
+    # VAD를 사용하여 말하는 구간 찾기
+    speech_timestamps = get_speech_ttimestamps(wav, model)
 
-# 결과 저장
-save_audio('voice_file/speech_only (1).wav', speech_wav, sampling_rate=16000)
+    # 말하는 구간만 추출
+    speech_wav = collect_chunks(speech_timestamps, wav)
+
+    # VAD 처리된 결과 저장
+    save_audio(output_filename, speech_wav, sampling_rate=16000)
+
+    duration_vad = time.time() - start_time_vad
+    print(f"VAD processing time: {duration_vad} seconds")
+
+
+    return duration_vad
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide the input audio file as a command line argument.")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_filename = input_file.replace(".wav", "_vad.wav")
+
+    vad(input_file, output_filename)
